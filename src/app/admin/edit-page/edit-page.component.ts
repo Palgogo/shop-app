2a4666c013673a802fbf78530666a10682627247
Pavel Maisiuk-Dranko <ipalgogo@gmail.com>
2020-07-20 13:48:19 +0300

    edit page works fine

--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -1,4 +1,9 @@
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ProductService } from './../../shared/product.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { Product } from 'src/app/shared/interfaces';
 
 @Component({
   selector: 'app-edit-page',
@@ -7,9 +12,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditPageComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup
+  product: Product
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.route.params
+    .pipe(
+      switchMap(params => {
+        return this.productService.getById(params['id'])
+      })
+    ).subscribe( product => {
+      this.product = product
+      this.form = new FormGroup({
+      type: new FormControl(this.product.type,Validators.required),
+      title: new FormControl(this.product.title,Validators.required),
+      photo: new FormControl(this.product.photo,Validators.required),
+      info: new FormControl(this.product.info,Validators.required),
+      price: new FormControl(this.product.price,Validators.required),
+      })
+    })
+  }
+
+  submit(){
+    if(this.form.invalid){
+      return
+    }
+
+
+    console.log(this.form)
+    this.productService.update({
+      ...this.product,
+      type: this.form.value.type,
+      title: this.form.value.title,
+      photo: this.form.value.photo,
+      info: this.form.value.info,
+      price: this.form.value.price,
+      date: new Date(),
+    }).subscribe(res =>{
+      this.router.navigate(['/admin','dashboard'])
+    })
   }
 
 }
