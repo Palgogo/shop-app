d8fda7b3cb466a6bc535acbd4435d056a1e65f64
Pavel Maisiuk-Dranko <ipalgogo@gmail.com>
2020-07-20 07:59:12 +0300

    add getting by id method

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Product } from './../shared/interfaces';
+import { ProductService } from './../shared/product.service';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product-page',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductPageComponent implements OnInit {
 
-  constructor() { }
+  @Input() product$
+  loading = false
+
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.product$ = this.route.params
+    .pipe(
+      switchMap(params => {
+        return this.productService.getById(params['id'])
+      })
+    )
   }
 
 }
